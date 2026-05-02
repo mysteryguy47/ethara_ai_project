e36bbb39167550ea75c2adc352ced1064b187c6c
mysteryguy47 <ayushkhurana47@gmail.com>
2026-05-02 14:37:00 +0530

    basic express server

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'taskflow api is running' })
+})
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
